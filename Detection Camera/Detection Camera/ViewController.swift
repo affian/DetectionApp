b824fd8e3e0a6e55ad78c47401637a2e695b215e
Nnamdi Affia <thegrandwizard@pc-65-76.customer.westgate.vita.lan>
2018-04-06 23:07:10 +0100

    ready to test with iPhone camera after update

--- Detection Camera/Detection Camera/ViewController.swift
+++ Detection Camera/Detection Camera/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video)
+            else
+        {
+            return
+            
+        }
+        
+        guard let input = try? AVCaptureDeviceInput (device: captureDevice)
+            else
+        
+        {
+            return
+        }
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
